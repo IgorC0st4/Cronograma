8b0c0c6b2e71d63ec07b4113a61863489205bfc3
Igor Carvalho <costa.igor0108@gmail.com>
2019-12-31 12:43:09 -0400

    dialog

--- app/src/main/java/br/com/cronograma/ui/DisciplinasFragment.java
+++ app/src/main/java/br/com/cronograma/ui/DisciplinasFragment.java
@@ -1,5 +1,6 @@
 package br.com.cronograma.ui;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,14 +8,39 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import br.com.cronograma.R;
 
-public class DisciplinasFragment extends Fragment {
+public class DisciplinasFragment extends Fragment implements View.OnClickListener {
+
+    private FloatingActionButton fab;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_disciplinas, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_disciplinas, container, false);
+        fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.dialog_disciplinas_crud, null))
+                .setPositiveButton(R.string.lbl_btn_salvar, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+        // Create the AlertDialog object and return it
+        builder.create();
     }
 }
